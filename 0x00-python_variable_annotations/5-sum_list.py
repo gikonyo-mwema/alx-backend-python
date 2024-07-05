175c44632953b24fb0fff08075ba9947792d1287
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-07-05 04:09:04 +0300

    sum of float list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Sum of floats """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Return the sum of float arguments """
+    return sum(input_list)
