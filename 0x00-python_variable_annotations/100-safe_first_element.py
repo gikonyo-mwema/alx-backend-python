8b050c7f47224851c7a5de9a0df56a19c004470a
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-07-05 05:02:43 +0300

    first element of input

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/bin/usr/env python3
+""" Duck typed annotations """
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ Returns the first element of the input, otherwise None"""
+    if lst:
+        return lst[0]
+    else:
+        return None
