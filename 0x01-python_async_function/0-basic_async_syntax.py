f6d93d3e844281d8a52d152a0c2dda18ce1ab80c
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-07-09 04:03:19 +0300

    float to int

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous coroutine that waits for a random delay
     """
