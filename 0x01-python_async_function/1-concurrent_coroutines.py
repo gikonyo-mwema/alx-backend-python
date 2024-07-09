46733df76bfda3a14d2388bbf2f48b810d996e9f
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-07-09 04:31:34 +0300

    pycodestyle fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,10 @@
 """
 List of all the delays.
 """
-
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: float) -> List[float]:
     """
