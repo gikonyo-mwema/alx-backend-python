d9aefc95d8eae978444ebe98ec9e8c0904cd1ee5
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-07-10 05:06:46 +0300

    list imported

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-""" Coroutine with no arguments """
-async_generator = __import__('1-async_generator').async_generator
+"""comprehension that takes no arg"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """
-    coroutine will collect 10 random numbers using an async
-    comprehensing over async_generator,
-    then return the 10 random numbers.
-
-    """
+    """coroutine that takes no arg"""
     return [x async for x in async_generator()]
